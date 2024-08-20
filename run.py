ccfe57e5b06202f749c2c375a81cbd0761d0e360
Maria Migrova <maria.migrova@gmail.com>
2024-08-20 21:06:25 +0000

    Add spacing between prints

--- run.py
+++ run.py
@@ -23,7 +23,7 @@ def update_goal():
     date = goal_sheet.col_values(1)[1]
     goal_amount = goal_sheet.col_values(3)[1]
 
-    print(f"Your current goal is to save {goal_amount} € by {date}.")
+    print(f"Your current goal is to save {goal_amount} € by {date}.\n")
 
     while True:
         goal_change = input(
@@ -33,36 +33,37 @@ def update_goal():
         if goal_change == "yes":
             while True:
                 while True:
-                    new_goal_amount = input("New savings goal amount (€):\n")
+                    new_goal_amount = input("\nNew savings goal amount (€):\n")
                     try:
                         new_goal_amount = float(new_goal_amount)
                         break
                     except ValueError:
-                        print("Invalid amount. Please enter a number.")
+                        print("\nInvalid amount. Please enter a number.\n")
 
                 while True:
                     new_goal_date = input(
-                        "New savings goal date: (dd/mm/yyyy):\n")
+                        "\nNew savings goal date: (dd/mm/yyyy):\n")
                     try:
                         new_goal_date_obj = datetime.strptime(
                             new_goal_date, '%d/%m/%Y')
                         today = datetime.today()
                         if new_goal_date_obj < today:
                             print(
-                                "Your saving goal date cannot be in the past. "
-                                "Please try again.")
+                                "\nYour saving goal date cannot be "
+                                "in the past. "
+                                "Please try again.\n")
                         else:
                             new_goal_date = new_goal_date_obj.strftime(
                                 "%d/%m/%Y")
                             break
                     except ValueError:
-                        print("Invalid date format. Please try again.")
+                        print("\nInvalid date format. Please try again.\n")
 
                 while True:
                     confirm_update = input(
-                        f"New goal: Save {new_goal_amount} €\n"
+                        f"\nNew goal: \nSave {new_goal_amount} €\n"
                         f"by {new_goal_date}.\n"
-                        "Are you happy with this update? ('yes'/'no'):\n"
+                        "\nAre you happy with this update? ('yes'/'no'):\n"
                     ).strip().lower()
 
                     if confirm_update == "yes":
@@ -76,21 +77,21 @@ def update_goal():
                         goal_sheet.update(
                             range_name='B2', values=[[week_number]])
 
-                        print("Goal updated successfully!")
+                        print("\nGoal updated successfully!\n")
                         return
 
                     elif confirm_update == "no":
-                        print("Let's try updating the goal again.")
+                        print("\nLet's try updating the goal again.\n")
                         break
 
                     else:
-                        print("Invalid input, please type 'yes' or 'no'.")
+                        print("\nInvalid input, please type 'yes' or 'no'.\n")
 
         elif goal_change == "no":
-            print("No changes made to your savings goal.")
+            print("\nNo changes made to your savings goal.\n")
             break
         else:
-            print("Invalid input, please type 'yes' or 'no'.")
+            print("\nInvalid input, please type 'yes' or 'no'.\n")
 
 
 def update_savings():
@@ -114,7 +115,7 @@ def update_savings():
     print(f"Total Income:    {total_income:.2f} €")
     print(f"Total Expenses:  {total_expense:.2f} €")
     print(f"Current Savings: {current_savings:.2f} €\n")
-    print("Updating the savings sheet...")
+    print("\nUpdating the savings sheet...\n")
 
     savings_sheet.append_row([
         date, datetime.now().isocalendar()[1], current_savings])
@@ -129,7 +130,9 @@ def add_new_income():
 
     while True:
         while True:
-            saving_date = input("Enter the date of the income (dd/mm/yyyy):\n")
+            saving_date = input(
+                "\nEnter the date of the income "
+                "(dd/mm/yyyy):\n")
             try:
                 saving_date_obj = datetime.strptime(saving_date, '%d/%m/%Y')
                 today = datetime.today()
@@ -137,20 +140,20 @@ def add_new_income():
 
                 if saving_date_obj > today:
                     print(
-                        "Your income date cannot be in the future. "
-                        "Please, try again.")
+                        "\nYour income date cannot be in the future. "
+                        "Please, try again.\n")
                 elif saving_date_obj < three_months_earlier:
                     print(
-                        "Your income date cannot be older than 3 months. "
-                        "Please, try again.")
+                        "\nYour income date cannot be older than 3 months. "
+                        "Please, try again.\n")
                 else:
                     saving_date = saving_date_obj.strftime("%d/%m/%Y")
                     break
             except ValueError:
-                print("Invalid date format. Please, try again.")
+                print("\nInvalid date format. Please, try again.\n")
 
         income_types = ["Wages", "Freelance", "Investment", "Other"]
-        print("Select the type of income:")
+        print("\nSelect the type of income:\n")
         for idx, income_type in enumerate(income_types, start=1):
             print(f"{idx}. {income_type}")
 
@@ -158,64 +161,65 @@ def add_new_income():
             try:
                 income_type_choice = int(
                     input(
-                        "Enter the number corresponding to the income type:\n")
+                        "\nEnter the number corresponding "
+                        "to the income type:\n")
                     )
                 if 1 <= income_type_choice <= len(income_types):
                     income_type = income_types[income_type_choice - 1]
                     break
                 else:
                     print(
-                        "Invalid choice. Please choose a number from the list."
+                        "\nInvalid choice. Please "
+                        "choose a number from the list.\n"
                         )
             except ValueError:
-                print("Invalid input. Please enter a number.")
+                print("\nInvalid input. Please enter a number.\n")
 
         while True:
-            saving_amount = input("Enter the amount of income (€):\n")
+            saving_amount = input("\nEnter the amount of income (€):\n")
             try:
                 saving_amount = float(saving_amount)
 
                 if saving_amount > 1000000:
                     print(
-                        "Your income amount cannot be more than 1000 000 €. "
+                        "\nYour income amount cannot be more than 1000 000 €. "
                         "Otherwise, you don't need the Saving App. 😂 "
-                        "Please, try again.")
+                        "Please, try again.\n")
                 else:
                     break
             except ValueError:
-                print("Invalid amount. Please enter a number.")
-                break
+                print("\nInvalid amount. Please enter a number.\n")
 
         week_number = datetime.strptime(
             saving_date, '%d/%m/%Y').isocalendar()[1]
 
         while True:
             check = input(
-                f"Date: {saving_date}\n"
+                f"\nDate: {saving_date}\n"
                 f"Income type: {income_type}\n"
                 f"Amount: {saving_amount}\n"
-                "Do you want to save this? ('yes'/'no'): ")
+                "\nDo you want to save this? ('yes'/'no'): \n")
             if check == "yes":
                 incomes_sheet.append_row(
                     [saving_date, week_number, income_type, saving_amount])
-                print("New income added successfully!\n")
+                print("\nNew income added successfully!\n")
                 break
             elif check == "no":
-                print("Income entry discarded. Please re-enter details.\n")
+                print("\nIncome entry discarded. Please re-enter details.\n")
                 break
             else:
-                print("Invalid input, please type 'yes' or 'no'.")
+                print("\nInvalid input, please type 'yes' or 'no'.\n")
 
         while True:
             more_incomes = input(
-                "Do you want to add another income? (yes/no):\n"
+                "\nDo you want to add another income? (yes/no):\n"
                 ).strip().lower()
             if more_incomes == 'yes':
                 break
             elif more_incomes == 'no':
                 return
             else:
-                print("Invalid input, please type 'yes' or 'no'.")
+                print("\nInvalid input, please type 'yes' or 'no'.\n")
 
 
 def add_new_expenses():
@@ -228,7 +232,7 @@ def add_new_expenses():
     while True:
         while True:
             expense_date = input(
-                "Enter the date of the expense (dd/mm/yyyy):\n")
+                "\nEnter the date of the expense (dd/mm/yyyy):\n")
             try:
                 expense_date_obj = datetime.strptime(expense_date, '%d/%m/%Y')
                 today = datetime.today()
@@ -236,85 +240,88 @@ def add_new_expenses():
 
                 if expense_date_obj > today:
                     print(
-                        "Your expense date cannot be in the future. "
-                        "Please try again.")
+                        "\nYour expense date cannot be in the future. "
+                        "Please try again.\n")
                 elif expense_date_obj < three_months_earlier:
                     print(
-                        "Your expense date cannot be older than 3 months. "
-                        "Please, try again.")
+                        "\nYour expense date cannot be older than 3 months. "
+                        "Please, try again.\n")
                 else:
                     expense_date = expense_date_obj.strftime("%d/%m/%Y")
                     break
             except ValueError:
-                print("Invalid date format. Please try again.")
+                print("\nInvalid date format. Please try again.\n")
 
         expenses_types = [
             "Housing", "Transport", "Food", "Cosmetics", "Health & Wellness",
             "Entertainment & Leisure", "Clothing & Personal Care", "Gifts"
             ]
-        print("Select the type of expenses:")
+        print("\nSelect the type of expenses:\n")
         for idx, expenses_type in enumerate(expenses_types, start=1):
             print(f"{idx}. {expenses_type}")
 
         while True:
             try:
                 expenses_type_choice = int(input(
-                    "Enter the number corresponding to the expenses type:\n"))
+                    "\nEnter the number corresponding "
+                    "to the expenses type:\n"))
                 if 1 <= expenses_type_choice <= len(expenses_types):
                     expenses_type = expenses_types[expenses_type_choice - 1]
                     break
                 else:
                     print(
-                        "Invalid choice. Please choose a number from the list."
+                        "\nInvalid choice. "
+                        "Please choose a number from the list.\n"
                         )
             except ValueError:
-                print("Invalid input. Please enter a number.")
+                print("\nInvalid input. Please enter a number.\n")
 
         while True:
-            expense_amount = input("Enter the amount of expenses (€):\n")
+            expense_amount = input("\nEnter the amount of expenses (€):\n")
             try:
                 expense_amount = float(expense_amount)
                 if expense_amount > 1000000:
                     print(
-                        "Your expense amount cannot be more than 1000 000 €. "
+                        "\nYour expense amount cannot be "
+                        "more than 1000 000 €. "
                         "Otherwise, you don't need the Saving App. 😂 "
-                        "Please, try again.")
+                        "Please, try again.\n")
                 else:
                     break
             except ValueError:
-                print("Invalid amount. Please enter a number.")
+                print("\nInvalid amount. Please enter a number.\n")
 
         week_number = datetime.strptime(
             expense_date, '%d/%m/%Y').isocalendar()[1]
 
         while True:
             check = input(
-                f"Date: {expense_date}\n"
+                f"\nDate: {expense_date}\n"
                 f"Expenses type: {expenses_type}\n"
                 f"Amount: {expense_amount}\n"
-                "Do you want to save this? ('yes'/'no'): ")
+                "\nDo you want to save this? ('yes'/'no'): \n")
             if check == "yes":
                 expenses_sheet.append_row(
                     [expense_date, week_number, expenses_type, expense_amount])
-                print("New expenses added successfully!\n")
+                print("\nNew expenses added successfully!\n")
                 break
             elif check == "no":
-                print("Expense entry discarded. Please re-enter details.\n")
+                print("\nExpense entry discarded. Please re-enter details.\n")
                 break
             else:
-                print("Invalid input, please type 'yes' or 'no'.")
+                print("\nInvalid input, please type 'yes' or 'no'.\n")
 
         while True:
             more_expenses = input(
-                "Do you want to add another expenses? (yes/no):\n"
+                "\nDo you want to add another expenses? (yes/no):\n"
                 ).strip().lower()
             if more_expenses == 'yes':
                 break
             elif more_expenses == 'no':
-                print("No more expenses to add.\n")
+                print("\nNo more expenses to add.\n")
                 return
             else:
-                print("Invalid input, please type 'yes' or 'no'.")
+                print("\nInvalid input, please type 'yes' or 'no'.\n")
 
 
 def calculate_goal_progress():
@@ -345,12 +352,14 @@ def calculate_goal_progress():
 
     if current_savings >= goal_amount:
         print("Congratulations! 🎉")
-        print(f"You have reached your savings goal of {goal_amount:.2f} €!")
+        print(f"You have reached your savings goal of {goal_amount:.2f} €!\n")
 
     if remaining_weeks > 0:
         weekly_savings_needed = missing_amount / remaining_weeks
-        print(f"Weeks Remaining:   {remaining_weeks}")
-        print(f"Average Weekly Savings Needed: {weekly_savings_needed:.2f} €")
+        print(
+            f"Weeks Remaining:   {remaining_weeks}")
+        print(
+            f"Average Weekly Savings Needed: {weekly_savings_needed:.2f} €\n")
     else:
         print(
             "The goal date has passed.",
@@ -375,10 +384,10 @@ def main():
         elif add_income_prompt == "no":
             break
         else:
-            print("Invalid input, please type 'yes' or 'no'.")
+            print("\nInvalid input, please type 'yes' or 'no'.\n")
 
     while True:
-        print("~~~EXPENSES~~~")
+        print("\n~~~EXPENSES~~~")
         add_expenses_prompt = input(
             "Do you want to add any new expenses? (yes/no):\n"
             ).strip().lower()
@@ -388,7 +397,7 @@ def main():
         elif add_expenses_prompt == "no":
             break
         else:
-            print("Invalid input, please type 'yes' or 'no'.")
+            print("\nInvalid input, please type 'yes' or 'no'.\n")
 
     update_savings()
     calculate_goal_progress()
